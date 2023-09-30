3f465f27215dfcad16460667fb293058cbd90ca2
Kangkan Jyoti Medhi <131618563+Kangkan30@users.noreply.github.com>
2023-09-30 12:14:08 +0530

    Find a person is adult or not .
    Using this program you can find a person is adult or not. ( conditional statements)

---
+++ find a person is adult or not.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf(" Enter age");
+	scanf("%d",& age);
+	if 
+	(age>18){
+	printf("Adult\n");
+	printf("They can vote\n");
+	printf("THANK YOU :)\n");
+	}
+	else{
+		printf("Not an Adult");
+	}
+	return 0;
+}
